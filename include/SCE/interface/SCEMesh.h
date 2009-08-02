5c5fa69621df37432cf03ebf164ef407e3efa07f
yno <yno@d5589c5c-db5c-46e9-ada4-dbbd3b4b0917>
2009-08-02 11:11:45 +0000

    Completed SCEMesh module (theoretically).
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/scengine/scengine/trunk@101 d5589c5c-db5c-46e9-ada4-dbbd3b4b0917

--- include/SCE/interface/SCEMesh.h
+++ include/SCE/interface/SCEMesh.h
@@ -17,22 +17,17 @@
  -----------------------------------------------------------------------------*/
 
 /* created: 31/07/2009
-   updated: 31/07/2009 */
+   updated: 02/08/2009 */
 
 #ifndef SCEMESH_H
 #define SCEMESH_H
 
-#include <SCE/interface/SCEGeometry.h>
 #include <SCE/core/SCECVertexBuffer.h>
+#include <SCE/interface/SCEGeometry.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-enum sce_emeshrendermode {
-    SCE_UNIFIED_VBO_RENDER_MODE = SCE_UNIFIED_VAO_RENDER_MODE + 42;
-};
-typedef enum sce_emeshrendermode SCE_EMeshRenderMode;
 
 enum sce_emeshbuildmode {
     SCE_INDEPENDANT_VERTEX_BUFFER = 1,
@@ -43,35 +38,65 @@ typedef enum sce_emeshbuildmode SCE_EMeshBuildMode;
 
 typedef struct sce_smesharray SCE_SMeshArray;
 struct sce_smesharray {
-    SCE_CVertexBuffer *vb;
+    SCE_CVertexBufferData data;
     SCE_SGeometryArrayUser auser;
     SCE_SListIterator it;
 };
 
+typedef struct sce_smeshbuffer SCE_SMeshBuffer;
+struct sce_smeshbuffer {
+    SCE_CVertexBuffer vb;
+    SCE_SListIterator it;
+};
+
 typedef struct sce_smesh SCE_SMesh;
+/**
+ * \brief Structure of a renderable mesh
+ * \sa SCE_SGeometry, SCE_CVertexBuffer
+ */
 struct sce_smesh {
     SCE_SGeometry *geom;
+    int canfree_geom;
+    SCEenum prim;               /**< Primitive type (SCE_TRIANGLES
+                                 * recommanded) */
     SCE_SList arrays;           /**< SCE_SMeshArray */
-    SCE_CIndexBuffer *ib;
+    SCE_SList buffers;          /**< SCE_SMeshBuffer */
+    SCE_CIndexBuffer ib;
+    int use_ib;
+    SCE_CBufferRenderMode rmode; /**< Render mode */
+    SCE_EMeshBuildMode bmode;   /**< Build mode */
 };
+
+int SCE_Init_Mesh (void);
+void SCE_Quit_Mesh (void);
 
 void SCE_Mesh_InitArray (SCE_SMeshArray*);
 SCE_SMeshArray* SCE_Mesh_CreateArray (void);
+void SCE_Mesh_ClearArray (SCE_SMeshArray*);
 void SCE_Mesh_DeleteArray (SCE_SMeshArray*);
+
+void SCE_Mesh_InitBuffer (SCE_SMeshBuffer*);
+/*SCE_SMeshBuffer* SCE_Mesh_CreateBuffer (void);*/
+void SCE_Mesh_ClearBuffer (SCE_SMeshBuffer*);
+/*void SCE_Mesh_DeleteBuffer (SCE_SMeshBuffer*);*/
 
 void SCE_Mesh_Init (SCE_SMesh*);
 SCE_SMesh* SCE_Mesh_Create (void);
 SCE_SMesh* SCE_Mesh_CreateFrom (SCE_SGeometry*);
 void SCE_Mesh_Delete (SCE_SMesh*);
 
-int SCE_Mesh_MakeFrom (SCE_SMesh*, SCE_SGeometry*);
+int SCE_Mesh_SetGeometry (SCE_SMesh*, SCE_SGeometry*, int);
+int SCE_Mesh_Build (SCE_SMesh*, SCE_EMeshBuildMode, SCE_CBufferRenderMode);
+int SCE_Mesh_AutoBuild (SCE_SMesh*, int);
+void SCE_Mesh_SetRenderMode (SCE_SMesh*, SCE_CBufferRenderMode);
 
-int SCE_Mesh_Build (SCE_SMesh*, SCE_EMeshBuildMode, SCE_EMeshRenderMode);
+int SCE_Mesh_Update (SCE_SMesh*);
 
-void SCE_Mesh_SetRenderMode (SCE_SMesh*, SCE_EMeshRenderMode);
+SCE_SMesh* SCE_Mesh_Load (const char*, int);
 
 void SCE_Mesh_Bind (SCE_SMesh*);
 void SCE_Mesh_Render (void);
+void SCE_Mesh_RenderInstanced (SCEuint);
 void SCE_Mesh_Unbind (void);
 
 #ifdef __cplusplus
--- src/interface/SCEMesh.c
+++ src/interface/SCEMesh.c
@@ -17,15 +17,50 @@
  -----------------------------------------------------------------------------*/
 
 /* created: 31/07/2009
-   updated: 31/07/2009 */
+   updated: 01/08/2009 */
 
 #include <SCE/SCEMinimal.h>
 
+#include <SCE/utils/SCEResource.h>
+#include <SCE/core/SCECSupport.h>
 #include <SCE/interface/SCEMesh.h>
+
+typedef void (*SCE_SMeshRenderFunc)(SCEenum);
+typedef void (*SCE_SMeshRenderInstancedFunc)(SCEenum, SCEuint);
+
+static int is_init = SCE_FALSE;
+
+static int resource_type = 0;
+
+static SCE_SMesh *mesh_bound = NULL;
+static SCE_SMeshRenderFunc render_func = NULL;
+static SCE_SMeshRenderInstancedFunc render_func_instanced = NULL;
+
+static void* SCE_Mesh_LoadResource (const char*, int, void*);
+
+int SCE_Init_Mesh (void)
+{
+    if (is_init)
+        return SCE_OK;
+    resource_type = SCE_Resource_RegisterType (SCE_FALSE,
+                                               SCE_Mesh_LoadResource, NULL);
+    if (resource_type < 0)
+        goto fail;
+    is_init = SCE_TRUE;
+    return SCE_OK;
+fail:
+    SCEE_LogSrc ();
+    SCEE_LogSrcMsg ("failed to initialize mesh module");
+    return SCE_ERROR;
+}
+void SCE_Quit_Mesh (void)
+{
+    is_init = SCE_FALSE;
+}
 
 void SCE_Mesh_InitArray (SCE_SMeshArray *marray)
 {
-    marray->vb = NULL;
+    SCE_CInitVertexBufferData (&marray->data);
     SCE_Geometry_InitArrayUser (&marray->auser);
     SCE_List_InitIt (&marray->it);
     SCE_List_SetData (&marray->it, marray);
@@ -39,20 +74,71 @@ SCE_SMeshArray* SCE_Mesh_CreateArray (void)
         SCE_Mesh_InitArray (marray);
     return marray;
 }
+static void SCE_Mesh_RemoveArray (SCE_SMeshArray*);
+void SCE_Mesh_ClearArray (SCE_SMeshArray *marray)
+{
+    SCE_CClearVertexBufferData (&marray->data);
+    SCE_Geometry_ClearArrayUser (&marray->auser);
+    SCE_Mesh_RemoveArray (marray);
+}
 void SCE_Mesh_DeleteArray (SCE_SMeshArray *marray)
 {
     if (marray) {
-        SCE_CDeleteVertexBuffer (marray->vb);
-        SCE_Geometry_ClearArrayUser (&marray->auser);
+        SCE_Mesh_ClearArray (marray);
         SCE_free (marray);
     }
 }
 
+void SCE_Mesh_InitBuffer (SCE_SMeshBuffer *mbuf)
+{
+    SCE_CInitVertexBuffer (&mbuf->vb);
+    SCE_List_InitIt (&mbuf->it);
+    SCE_List_SetData (&mbuf->it, mbuf);
+}
+#if 0
+SCE_SMeshBuffer* SCE_Mesh_CreateBuffer (void)
+{
+    SCE_SMeshBuffer *mbuf = NULL;
+    return mbuf;
+}
+#endif
+static void SCE_Mesh_RemoveBuffer (SCE_SMeshBuffer*);
+void SCE_Mesh_ClearBuffer (SCE_SMeshBuffer *mbuf)
+{
+    SCE_CClearVertexBuffer (&mbuf->vb);
+    SCE_Mesh_RemoveBuffer (mbuf);
+}
+#if 0
+void SCE_Mesh_DeleteBuffer (SCE_SMeshBuffer *buf)
+{
+    if (buf) {
+        SCE_Mesh_ClearBuffer (buf);
+        SCE_free (buf);
+    }
+}
+#endif
+
+static void SCE_Mesh_FreeArray (void *marray)
+{
+    SCE_Mesh_RemoveArray (marray);
+}
+static void SCE_Mesh_FreeBuffer (void *mbuf)
+{
+    SCE_Mesh_RemoveBuffer (mbuf);
+}
 void SCE_Mesh_Init (SCE_SMesh *mesh)
 {
     mesh->geom = NULL;
+    mesh->canfree_geom = SCE_FALSE;
+    mesh->prim = SCE_POINTS;    /* do not take any risk. */
     SCE_List_Init (&mesh->arrays);
-    mesh->ib = NULL;
+    SCE_List_SetFreeFunc (&mesh->arrays, SCE_Mesh_FreeArray);
+    SCE_List_Init (&mesh->buffers);
+    SCE_List_SetFreeFunc (&mesh->buffers, SCE_Mesh_FreeBuffer);
+    SCE_CInitIndexBuffer (&mesh->ib);
+    mesh->use_ib = SCE_FALSE;
+    mesh->rmode = SCE_VA_RENDER_MODE;
+    mesh->bmode = 0;            /* TODO: will do. */
 }
 SCE_SMesh* SCE_Mesh_Create (void)
 {
@@ -74,27 +160,386 @@ SCE_SMesh* SCE_Mesh_CreateFrom (SCE_SGeometry *geom)
     }
     return mesh;
 }
+void SCE_Mesh_Clear (SCE_SMesh *mesh)
+{
+    SCE_CClearIndexBuffer (&mesh->ib);
+    SCE_List_Clear (&mesh->arrays);
+    SCE_List_Clear (&mesh->buffers);
+}
 void SCE_Mesh_Delete (SCE_SMesh *mesh)
 {
     if (mesh) {
-        SCE_CDeleteIndexBuffer (mesh->ib);
-        SCE_List_Clear (&mesh->arrays);
+        SCE_Mesh_Clear (mesh);
         SCE_free (mesh);
     }
 }
 
-int SCE_Mesh_MakeFrom (SCE_SMesh *mesh, SCE_SGeometry *geom)
+
+static void SCE_Mesh_UpdateArrayCallback (void *data, size_t *range)
 {
+    SCE_CModifiedVertexBufferData (data, range);
+}
+/**
+ * \internal
+ * \brief Sets the geometry array of a mesh array
+ * \sa SCE_Mesh_AddArray(), SCE_Mesh_AddArrayFrom()
+ */
+static void SCE_Mesh_SetArrayArray (SCE_SMeshArray *marray,
+                                    SCE_SGeometryArray *array,
+                                    unsigned int n_vertices)
+{
+    SCE_CSetVertexBufferDataArrayData (&marray->data,
+                                       SCE_Geometry_GetArrayData (array),
+                                       n_vertices);
+    SCE_Geometry_AddUser (array, &marray->auser, SCE_Mesh_UpdateArrayCallback,
+                          &marray->data);
 }
 
+
+/**
+ * \internal
+ */
+static void SCE_Mesh_AddArray (SCE_SMesh *mesh, SCE_SMeshArray *marray)
+{
+/*     if (marray->mesh) */
+    SCE_List_Appendl (&mesh->arrays, &marray->it);
+}
+/**
+ * \internal
+ */
+static SCE_SMeshArray* SCE_Mesh_AddNewArray (SCE_SMesh *mesh)
+{
+    SCE_SMeshArray *marray = NULL;
+    if (!(marray = SCE_Mesh_CreateArray ()))
+        SCEE_LogSrc ();
+    else
+        SCE_Mesh_AddArray (mesh, marray);
+    return marray;
+}
+/**
+ * \internal
+ */
+static SCE_SMeshArray* SCE_Mesh_AddNewArrayFrom (SCE_SMesh *mesh,
+                                                 SCE_SGeometryArray *array,
+                                                 unsigned int n_vertices)
+{
+    SCE_SMeshArray *marray = NULL;
+    if (!(marray = SCE_Mesh_AddNewArray (mesh)))
+        SCEE_LogSrc ();
+    else
+        SCE_Mesh_SetArrayArray (marray, array, n_vertices);
+    return marray;
+}
+/**
+ * \internal
+ */
+static void SCE_Mesh_RemoveArray (SCE_SMeshArray *marray)
+{
+    SCE_List_Remove (&marray->it);
+}
+
+/**
+ * \brief Sets the geometry for a mesh
+ * \sa SCE_Mesh_CreateFrom(), SCE_Mesh_Build(), SCE_SGeometry
+ */
+int SCE_Mesh_SetGeometry (SCE_SMesh *mesh, SCE_SGeometry *geom, int canfree)
+{
+    unsigned int n_vertices = 0;
+    SCE_SGeometryArray *index_array = NULL;
+    SCE_SListIterator *it = NULL;
+    SCE_SList *arrays = NULL;
+
+    n_vertices = SCE_Geometry_GetNumVertices (geom);
+    arrays = SCE_Geometry_GetArrays (geom);
+    SCE_List_ForEach (it, arrays) {
+        if (!SCE_Mesh_AddNewArrayFrom (mesh, SCE_List_GetData (it), n_vertices))
+            goto fail;
+    }
+    arrays = SCE_Geometry_GetModifiedArrays (geom);
+    SCE_List_ForEach (it, arrays) {
+        if (!SCE_Mesh_AddNewArrayFrom (mesh, SCE_List_GetData (it), n_vertices))
+            goto fail;
+    }
+    index_array = SCE_Geometry_GetIndexArray (geom);
+    if (index_array) {
+        /* NOTE: not very beautiful... */
+        SCE_CIndexArray ia;
+        SCE_CVertexArrayData *vdata = SCE_Geometry_GetArrayData (index_array);
+        ia.type = vdata->type;
+        ia.data = vdata->data;
+        SCE_CSetIndexBufferIndexIndexArray (&mesh->ib, ia,
+                                            SCE_Geometry_GetNumIndices (geom));
+        mesh->use_ib = SCE_TRUE;
+    }
+    mesh->geom = geom;
+    mesh->canfree_geom = canfree;
+    return SCE_OK;
+fail:
+    SCEE_LogSrc ();
+    return SCE_ERROR;
+}
+
+
+/**
+ * \internal
+ */
+static void SCE_Mesh_AddBufferArray (SCE_SMeshBuffer *mbuf,
+                                     SCE_SMeshArray *marray)
+{
+    SCE_CAddVertexBufferData (&mbuf->vb, &marray->data);
+}
+
+/**
+ * \internal
+ * \brief Adds a buffer to a mesh
+ * \sa SCE_Mesh_RemoveBuffer()
+ */
+static void SCE_Mesh_AddBuffer (SCE_SMesh *mesh, SCE_SMeshBuffer *mbuf)
+{
+    SCE_List_Appendl (&mesh->buffers, &mbuf->it);
+}
+/**
+ * \internal
+ * \brief Adds a new buffer to a mesh
+ */
+static SCE_SMeshBuffer* SCE_Mesh_AddNewBuffer (SCE_SMesh *mesh)
+{
+    SCE_SMeshBuffer *mbuf = NULL;
+    if (!(mbuf = SCE_Mesh_CreateBuffer ()))
+        SCEE_LogSrc ();
+    else
+        SCE_Mesh_AddBuffer (mesh, mbuf);
+    return mbuf;
+}
+/**
+ * \internal
+ * \brief Adds a new buffer to a mesh from a mesh array
+ */
+static SCE_SMeshBuffer* SCE_Mesh_AddNewBufferFrom (SCE_SMesh *mesh,
+                                                   SCE_SMeshArray *marray)
+{
+    SCE_SMeshBuffer *mbuf = NULL;
+    if (!(mbuf = SCE_Mesh_AddNewBuffer (mesh)))
+        SCEE_LogSrc ();
+    else
+        SCE_Mesh_AddBufferArray (mbuf, marray);
+    return mbuf;
+}
+/**
+ * \internal
+ * \brief Removes a buffer from a mesh
+ * \sa SCE_Mesh_AddBuffer()
+ */
+static void SCE_Mesh_RemoveBuffer (SCE_SMeshBuffer *mbuf)
+{
+    SCE_List_Remove (&mbuf->it);
+}
+
+
+static int SCE_Mesh_MakeIndependantVB (SCE_SMesh *mesh)
+{
+    SCE_SListIterator *it = NULL;
+    SCE_List_ForEach (it, &mesh->arrays) {
+        if (!SCE_Mesh_AddNewBufferFrom (mesh, SCE_List_GetData (it)))
+            goto fail;
+    }
+    return SCE_OK;
+fail:
+    SCEE_LogSrc ();
+    return SCE_ERROR;
+}
+static int SCE_Mesh_MakeGlobalVB (SCE_SMesh *mesh)
+{
+    SCE_SListIterator *it = NULL;
+    SCE_SMeshBuffer *mbuf = NULL;
+
+    if (!(mbuf = SCE_Mesh_AddNewBuffer (mesh))) {
+        SCEE_LogSrc ();
+        return SCE_ERROR;
+    }
+    SCE_List_ForEach (it, &mesh->arrays)
+        SCE_Mesh_AddBufferArray (mbuf, SCE_List_GetData (it));
+    return SCE_OK;
+}
+
+static void SCE_Mesh_BuildBuffers (SCE_SMesh *mesh, SCE_CBufferRenderMode mode)
+{
+    SCE_SListIterator *it = NULL;
+
+    if (mode != SCE_UNIFIED_VBO_RENDER_MODE) {
+        SCE_List_ForEach (it, &mesh->buffers) {
+            SCE_SMeshBuffer *mbuf = SCE_List_GetData (it);
+            SCE_CBuildVertexBuffer (&mbuf->vb, mode);
+        }
+    } else {
+        SCE_SMeshBuffer *rootbuf = NULL;
+        SCE_SListIterator *it2 = NULL;
+        it = SCE_List_GetFirst (&mesh->buffers);
+        rootbuf = SCE_List_GetData (it);
+        it = it2 = SCE_List_GetNext (it);
+        SCE_List_ForEachNext (it) {
+            SCE_SMeshBuffer *mbuf = SCE_List_GetData (it);
+            SCE_CBuildVertexBuffer (&mbuf->vb, SCE_VBO_RENDER_MODE);
+        }
+        SCE_CBuildVertexBuffer (&rootbuf->vb, mode);
+        SCE_List_ForEachNext (it2)
+            SCE_CUseVertexBuffer (SCE_List_GetData (it));
+        SCE_CFinishVertexBufferRender (); /* it calls CEndVertexArraySequence */
+    }
+}
 int SCE_Mesh_Build (SCE_SMesh *mesh, SCE_EMeshBuildMode bmode,
-                    SCE_EMeshRenderMode rmode)
+                    SCE_CBufferRenderMode rmode)
 {
-
+    int err = SCE_ERROR;
+    switch (bmode) {
+    case SCE_INDEPENDANT_VERTEX_BUFFER:
+        err = SCE_Mesh_MakeIndependantVB (mesh);
+        break;
+    case SCE_GLOBAL_VERTEX_BUFFER:
+        err = SCE_Mesh_MakeGlobalVB (mesh);
+        break;
+    }
+    if (err < 0)
+        goto fail;
+    SCE_Mesh_BuildBuffers (mesh, rmode);
+    mesh->bmode = bmode;
+    mesh->rmode = rmode;
+    return SCE_OK;
+fail:
+    SCEE_LogSrc ();
+    return SCE_ERROR;
 }
 
-void SCE_Mesh_SetRenderMode (SCE_SMesh*, SCE_EMeshRenderMode);
+/**
+ * \brief Uses default value for mesh build mode and determine the best buffer
+ * render mode
+ *
+ * It is recommanded to use this function to build meshes so the SCEngine can
+ * do some optimization under data structure to prevent for rendering
+ * situations that needs specific mesh data structure.
+ * \sa SCE_Mesh_Build()
+ */
+int SCE_Mesh_AutoBuild (SCE_SMesh *mesh, int mode)
+{
+    /* TODO: use mode to determine what kind of vertex buffer structure to use*/
+    if (SCE_CHasCap (SCE_VAO))
+        return SCE_Mesh_Build (mesh, SCE_INDEPENDANT_VERTEX_BUFFER,
+                               SCE_UNIFIED_VAO_RENDER_MODE);
+    else
+        return SCE_Mesh_Build (mesh, SCE_INDEPENDANT_VERTEX_BUFFER,
+                               SCE_VBO_RENDER_MODE);
+}
 
-void SCE_Mesh_Bind (SCE_SMesh*);
-void SCE_Mesh_Render (void);
-void SCE_Mesh_Unbind (void);
+/* why change mode on live? */
+#if 0
+void SCE_Mesh_SetRenderMode (SCE_SMesh *mesh, SCE_CBufferRenderMode rmode)
+{
+}
+#endif
+
+/**
+ * \brief Takes new (if any) arrays from the geometry of a mesh, and rebuild it
+ * \sa SCE_Mesh_SetGeometry(), SCE_Mesh_Build()
+ */
+int SCE_Mesh_Update (SCE_SMesh *mesh)
+{
+    /* not yet implemented, trop difficulte. (et trop chiant) */
+}
+
+/**
+ * \internal
+ */
+static void* SCE_Mesh_LoadResource (const char *fname, int force, void *data)
+{
+    SCE_SMesh *mesh = NULL;
+    SCE_SGeometry *geom = NULL;
+    (void)data;
+
+    if (!(mesh = SCE_Mesh_Create ()))
+        goto fail;
+    if (force > 0)
+        force--;
+    if (!(geom = SCE_Geometry_Load (fname, force)))
+        goto fail;
+    if (SCE_Mesh_SetGeometry (mesh, geom, SCE_TRUE) < 0)
+        goto fail;
+    return mesh;
+fail:
+    SCE_Mesh_Delete (mesh);
+    SCEE_LogSrc ();
+    return NULL;
+}
+/**
+ * \brief Loads a mesh from a file containing geometry
+ * \param fname name of the geometry file
+ * \param force see SCE_Resource_Load()
+ * \returns a new or existing mesh, depends on resource availability
+ * and on \p force
+ *
+ * This function loads a mesh resource by loading a geometry resource using
+ * SCE_Geometry_Load() and set it to the mesh. This function doesn't build
+ * the returned mesh itself, so you have to do it before using the mesh.
+ * \sa SCE_Mesh_SetGeometry(), SCE_Geometry_Load(), SCE_Resource_Load()
+ */
+SCE_SMesh* SCE_Mesh_Load (const char *fname, int force)
+{
+    return SCE_Resource_Load (resource_type, fname, force);
+}
+
+
+/**
+ * \brief Declares a mesh as activated for rendering
+ * \sa SCE_Mesh_Render(), SCE_Mesh_RenderInstanced(), SCE_Mesh_Unbind()
+ */
+void SCE_Mesh_Bind (SCE_SMesh *mesh)
+{
+    if (mesh->rmode == SCE_UNIFIED_VBO_RENDER_MODE) {
+        SCE_SMeshBuffer *mbuf = NULL;
+        mbuf = SCE_List_GetData (SCE_List_GetFirst (&mesh->buffers));
+        SCE_CUseVertexBuffer (&mbuf->vb);
+    } else {
+        SCE_SListIterator *it = NULL;
+        SCE_List_ForEach (it, &mesh->buffers) {
+            SCE_SMeshBuffer *mbuf = SCE_List_GetData (it);
+            SCE_CUseVertexBuffer (&mbuf->vb);
+        }
+    }
+    if (mesh->use_ib) {
+        SCE_CUseIndexBuffer (&mesh->ib);
+        render_func = SCE_CRenderVertexBufferIndexed;
+        render_func_instanced = SCE_CRenderVertexBufferIndexedInstanced;
+    } else {
+        render_func = SCE_CRenderVertexBuffer;
+        render_func_instanced = SCE_CRenderVertexBufferInstanced;
+    }
+    mesh_bound = mesh;
+}
+/**
+ * \brief Render an instance of the mesh bound with SCE_Mesh_Bind()
+ * \sa SCE_Mesh_Bind(), SCE_Mesh_RenderInstanced(), SCE_Mesh_Unbind()
+ */
+void SCE_Mesh_Render (void)
+{
+    render_func (mesh_bound->prim);
+}
+/**
+ * \brief Render \p n_instances instances of the mesh bound with
+ * SCE_Mesh_Bind() using hardware geometry instancing
+ * \sa SCE_Mesh_Bind(), SCE_Mesh_Render(), SCE_Mesh_Unbind()
+ */
+void SCE_Mesh_RenderInstanced (SCEuint n_instances)
+{
+    render_func_instanced (mesh_bound->prim, n_instances);
+}
+/**
+ * \brief Kicks the bound mesh
+ * \note Useless in a pure GL 3 context
+ * \sa SCE_Mesh_Bind(), SCE_Mesh_Render(), SCE_Mesh_RenderInstanced(),
+ * SCE_CFinishVertexBufferRender()
+ */
+void SCE_Mesh_Unbind (void)
+{
+    if (SCE_TRUE/*non_full_gl3*/)
+        SCE_CFinishVertexBufferRender ();
+    mesh_bound = NULL;
+}
