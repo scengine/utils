c063b09346a36528e86cc5fcd86f2478c297a40e
ban <ban@d5589c5c-db5c-46e9-ada4-dbbd3b4b0917>
2009-08-06 18:27:46 +0000

    Improved deprecation finder tool.
     * Now it checks for uses of deprecations too;
     * Possibly colored output;
     * Better internal design;
     * ...and more.
    
    
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/scengine/scengine/trunk@132 d5589c5c-db5c-46e9-ada4-dbbd3b4b0917

--- devtools/list_deprecated.py
+++ devtools/list_deprecated.py
@@ -13,6 +13,7 @@ import os
 import re
 
 VERBOSE = False
+COLORS = True
 
 def printerr (*args):
   for s in args:
@@ -26,6 +27,32 @@ def printv (*args):
       sys.stdout.write (str (s))
     sys.stdout.write ('\n')
 
+class OutputState ():
+  
+  __STATES = { 'default' : '\033[m',
+               'failure' : '\033[31m',
+               'success' : '\033[32m' }
+  __use_colors = True
+  
+  def __init__ (self, use_colors=True):
+    self.__use_colors = use_colors
+  
+  def __set_status (self, status):
+    if self.__use_colors:
+      sys.stdout.write (self.__STATES[status])
+  
+  def set_failure (self):
+    self.__set_status ('failure')
+  def set_error (self):
+    self.set_failure ()
+  def set_success (self):
+    self.__set_status ('success')
+  def set_ok (self):
+    self.set_success ()
+  def reset (self):
+    self.__set_status ('default')
+    
+
 def clean_list (list):
   i = 0
   while True:
@@ -37,6 +64,19 @@ def clean_list (list):
         i += 1
     except IndexError:
       break
+
+class DeprecatedEntry ():
+  def __init__ (self):
+    self.deprecated_line    = 0     # line where @deprecated was found
+    self.function_line      = 0     # starting line of the deprecated function
+    self.prototype_line     = 0     # prototype line of the deprecated function
+    self.is_doc_deprecated  = False # whether the deprecation is in doc
+    self.is_code_deprecated = False # whether the deprecation is in code
+    self.function           = '???' # deprecated function name
+    self.source_file        = '???' # file containing deprecated function
+    self.header_file        = '???' # file containing deprecated prototype
+    self.n_uses             = 0     # number of uses in the code
+    self.uses               = []    # list of (file, line) entries of uses cases
 
 # FIXME: do a real parsing, not a RE-based one for better detection
 def find_deprecated (filename):
@@ -52,21 +92,26 @@ def find_deprecated (filename):
     func_re = re.compile (r'[^ ].*[ \t]([a-zA-Z0-9_]+)[ \t]*\([^\)].*')
     for line in fp:
       if dep_re.match (line):
-        found_line = line_num
-        function = '???'
-        #~ print '%s:%d::%s()' % (filename, line_num, function)
+        deprecated = DeprecatedEntry ()
+        deprecated.source_file = filename
+        deprecated.deprecated_line = line_num
+        deprecated.is_doc_deprecated = True
+        deprecated.function = '???'
+        #~ print '%s:%d::%s()' % (deprecated.filename, deprecated.deprecated_line, deprecated.function)
         for line in fp:
           line_num += 1
           res = func_re.match (line)
           if res:
-            function = res.group (1)
+            deprecated.function = res.group (1)
             break
           elif dep_re.match (line):
             raise ValueError ('Unrecognised input at line %d, deprecation found '
                               'before function name.' % line_num)
+        deprecated.function_line = line_num
+        deprecateds.append (deprecated)
         printv ('  %s:%d::%s() (deprecated at line %d)' %
-                (filename, line_num, function, found_line))
-        deprecateds.append (function)
+                (deprecated.source_file, deprecated.function_line,
+                 deprecated.function, deprecated.deprecated_line))
       line_num += 1
     fp.close ()
   
@@ -74,7 +119,6 @@ def find_deprecated (filename):
 
 def check_deprecated (filename, deprecateds):
   n_matches = 0
-  n_wrong = 0
   
   try:
     fp = open (filename)
@@ -86,11 +130,10 @@ def check_deprecated (filename, deprecateds):
     for line in fp:
       for dep in deprecateds:
         #~ print dep
-        if re.search (dep+'[ \t(]', line):
+        if re.search (dep.function+'[ \t(]', line):
           #~ print 'deprecation found on %s:%d' % (filename, line_num)
           n_matches += 1
           found = False
-          dep_line = line_num
           res = func_re.match (line)
           if res:
             if res.group (1):
@@ -103,17 +146,44 @@ def check_deprecated (filename, deprecateds):
                 if res.group (1):
                   found = True
                 break
-          if not found:
+          
+          dep.header_file = filename
+          dep.prototype_line = line_num
+          dep.is_code_deprecated = found
+          
+          if not dep.is_code_deprecated:
             printerr ('  %s:%d::%s() not marked as deprecated' %
-                      (filename, dep_line, dep))
-            n_wrong += 1
+                      (dep.header_file, dep.prototype_line, dep.function))
           else:
-            printv ('  OK for %s:%d::%s()' % (filename, dep_line, dep))
+            printv ('  OK for %s:%d::%s()' %
+                    (dep.header_file, dep.prototype_line, dep.function))
       line_num += 1
   
-  return (n_matches, n_wrong)
+  return n_matches
 
-def walk (deprecateds, dirname, fnames):
+def count_deprecated (filename, deprecateds):
+  n_matches = 0
+  
+  try:
+    fp = open (filename)
+  except IOError, ex:
+    raise ex
+  else:
+    line_num = 1
+    for line in fp:
+      for dep in deprecateds:
+        #~ print dep
+        if re.search ('[^a-zA-Z0-9]'+dep.function+'[^a-zA-Z0-9]', line):
+          printv ('  Use of deprecated entry %s found at %s:%d' %
+                  (dep.function, filename, line_num))
+          dep.n_uses += 1
+          dep.uses.append ((filename, line_num))
+          n_matches += 1
+      line_num += 1
+  
+  return n_matches
+
+def walk_find (deprecateds, dirname, fnames):
   #~ print dirname
   
   clean_list (fnames)
@@ -129,7 +199,7 @@ def walk (deprecateds, dirname, fnames):
       except ValueError, ex:
         printerr ("* Invalid data in '%s':" % file, ex)
 
-def walk2 (args, dirname, fnames):
+def walk_check (args, dirname, fnames):
   #~ print dirname
   
   clean_list (fnames)
@@ -139,20 +209,38 @@ def walk2 (args, dirname, fnames):
       #~ print f
       file = os.path.join (dirname, f)
       try:
-        result = check_deprecated (file, args[0])
-        args[1] += result[0]
-        args[2] += result[1]
+        args[1] += check_deprecated (file, args[0])
+      except IOError, ex:
+        printerr ("* I/O error when trying to read '%s':" % file, ex)
+
+def walk_count (deprecateds, dirname, fnames):
+  #~ print dirname
+  
+  clean_list (fnames)
+  ext_re = re.compile (r'.*\.[ch]$')
+  for f in fnames:
+    if ext_re.match (f):
+      #~ print f
+      file = os.path.join (dirname, f)
+      try:
+        n = count_deprecated (file, deprecateds)
+        printv ('  %d deprecated occurrence(s) in %s' % (n, file))
       except IOError, ex:
         printerr ("* I/O error when trying to read '%s':" % file, ex)
 
 def main (args=()):
   global VERBOSE
+  global COLORS
   
   for arg in args[1:]:
     if arg == '-v':
       VERBOSE = True
     elif arg == '-V':
       VERBOSE = False
+    elif arg == '-c':
+      COLORS = True
+    elif arg == '-C':
+      COLORS = False
     else:
       break
     args.remove (arg)
@@ -162,15 +250,58 @@ def main (args=()):
   
   deprecateds = []
   print ('Searching for deprecated functions in the documentation...')
-  os.path.walk (root_srcdir, walk, deprecateds)
+  os.path.walk (root_srcdir, walk_find, deprecateds)
   n_deprecateds = len (deprecateds)
   print ('Done, %d deprecation(s) found.' % (n_deprecateds))
   if len (deprecateds) > 0:
     print ('Checking for corresponding code deprecations...')
-    args = [deprecateds, 0, 0]
-    os.path.walk (root_headir, walk2, args)
+    args = [deprecateds, 0]
+    os.path.walk (root_headir, walk_check, args)
+    n_missing_dep = 0
+    for dep in deprecateds:
+      if not dep.is_code_deprecated:
+        n_missing_dep += 1
     print ('Done, %d matches found (%d missing), %d missing deprecation(s).' % 
-          (args[1], n_deprecateds - args[1], args[2]))
+          (args[1], n_deprecateds - args[1], n_missing_dep))
+    
+    print ('Searching deprecated uses...')
+    os.path.walk (root_srcdir, walk_count, deprecateds)
+    os.path.walk (root_headir, walk_count, deprecateds)
+    n_uses = 0
+    for dep in deprecateds:
+      n_uses += dep.n_uses
+    print ('Done, %d total deprecated uses (including declaration and definition)' %
+           (n_uses))
+    
+    print
+    print (' -- Final results -- ')
+    print
+    
+    output_status = OutputState (COLORS)
+    
+    max_uses_ok = 2
+    for dep in deprecateds:
+      uses_ok = True if dep.n_uses <= max_uses_ok else False
+      status_ok = True if uses_ok and dep.is_code_deprecated else False
+      status = 'OK' if status_ok else 'bad!'
+      if status_ok:
+        output_status.set_ok ()
+      else:
+        output_status.set_failure ()
+      
+      print ('%s(): %s' % (dep.function, status))
+      output_status.reset ()
+      if not dep.is_code_deprecated:
+        print ('  * Not marked as deprecated (should be done at %s:%d)' %
+               (dep.header_file, dep.prototype_line))
+      if uses_ok:
+        printv ('  * Uses are OK, guess the %d use(s) are declaration and/or definition' %
+                (dep.n_uses))
+      else:
+        print ('  * Used %d time(s) (including declaration and definition):' %
+               (dep.n_uses))
+        for use in dep.uses:
+          print ('    used at %s:%d' % (use[0], use[1]))
   
   return 0
 
