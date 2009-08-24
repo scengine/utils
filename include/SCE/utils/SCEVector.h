43606ad622ef55a98952351e32372cae772ff751
yno <yno@d5589c5c-db5c-46e9-ada4-dbbd3b4b0917>
2009-08-24 14:34:47 +0000

    Added functions Vector3_GetMin() and GetMax().
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/scengine/scengine/trunk@214 d5589c5c-db5c-46e9-ada4-dbbd3b4b0917

--- include/SCE/utils/SCEVector.h
+++ include/SCE/utils/SCEVector.h
@@ -123,6 +123,9 @@ void SCE_Vector3_RotateX (SCE_TVector3, float, float);
 void SCE_Vector3_RotateY (SCE_TVector3, float, float);
 void SCE_Vector3_RotateZ (SCE_TVector3, float, float);
 
+void SCE_Vector3_GetMin (SCE_TVector3, SCE_TVector3, SCE_TVector3);
+void SCE_Vector3_GetMax (SCE_TVector3, SCE_TVector3, SCE_TVector3);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
--- src/utils/SCEVector.c
+++ src/utils/SCEVector.c
@@ -16,8 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -----------------------------------------------------------------------------*/
  
-/* Cree le : 21 decembre 2006
-   derniere modification le 22/09/2007 */
+/* created: 21/12/2006
+   updated: 22/09/2007 */
 
 #include <SCE/utils/SCEMath.h>
 #include <SCE/utils/SCEVector.h>
@@ -26,45 +26,34 @@
 /**
  * \file SCEVector.c
  * \copydoc vector
- * 
  * \file SCEVector.h
  * \copydoc vector
  */
 
 /**
- * \defgroup vector Vectors transformations
+ * \defgroup vector Vectors
  * \ingroup utils
  * \brief Vector transformations functions
  */
 
 /** @{ */
 
-
-/* revise le 22/09/2007 */
 /**
- * \brief Normalize a vector
- * \param v a SCE_TVector3 to normalize
- * 
- * Normalize a SCE_TVector3
+ * \brief Normalizes a vector
  */
 void SCE_Vector3_Normalize (SCE_TVector3 v)
 {
     float r = SCE_Math_InvSqrt ((v[0]*v[0])+(v[1]*v[1])+(v[2]*v[2]));
     SCE_Vector3_Operator1 (v, *=, r);
 }
-/* revise le 22/09/2007 */
 /**
- * \brief Normalize a vector
- * \param v a SCE_TVector2 to normalize
- * 
- * Normalize a SCE_TVector2
+ * \brief Normalizes a vector
  */
 void SCE_Vector2_Normalize (SCE_TVector2 v)
 {
     float r = SCE_Math_InvSqrt ((v[0]*v[0])+(v[1]*v[1]));
     SCE_Vector2_Operator1 (v, *=, r);
 }
-
 
 /**
  * \brief Rotate a 3D vector through the X axis
@@ -84,7 +73,6 @@ void SCE_Vector3_RotateX (SCE_TVector3 v, float cos_, float sin_)
     v[1] = (vec[1] * cos_) - (vec[2] * sin_);
     v[2] = (vec[1] * sin_) + (vec[2] * cos_);
 }
-
 /**
  * \brief Rotate a 3D vector through the Y axis
  * \param v a SCE_TVector3 to rotate
@@ -103,7 +91,6 @@ void SCE_Vector3_RotateY (SCE_TVector3 v, float cos_, float sin_)
     v[0] = (vec[0] * cos_) + (vec[2] * sin_);
     v[2] = (vec[2] * cos_) - (vec[0] * sin_);
 }
-
 /**
  * \brief Rotate a 3D vector through the Z axis
  * \param v a SCE_TVector3 to rotate
@@ -123,5 +110,25 @@ void SCE_Vector3_RotateZ (SCE_TVector3 v, float cos_, float sin_)
     v[1] = (vec[0] * sin_) + (vec[1] * cos_);
 }
 
+/**
+ * \brief Constructs a vector with the minimal values of each other's ones
+ * \param newv the new vector (can be \p v1 or \p v2)
+ */
+void SCE_Vector3_GetMin (SCE_TVector3 newv, SCE_TVector3 v1, SCE_TVector3 v2)
+{
+    newv[0] = min (v1[0], v2[0]);
+    newv[1] = min (v1[1], v2[1]);
+    newv[2] = min (v1[2], v2[2]);
+}
+/**
+ * \brief Constructs a vector with the maximum values of each other's ones
+ * \param newv the new vector (can be \p v1 or \p v2)
+ */
+void SCE_Vector3_GetMax (SCE_TVector3 newv, SCE_TVector3 v1, SCE_TVector3 v2)
+{
+    newv[0] = max (v1[0], v2[0]);
+    newv[1] = max (v1[1], v2[1]);
+    newv[2] = max (v1[2], v2[2]);
+}
 
 /** @} */
